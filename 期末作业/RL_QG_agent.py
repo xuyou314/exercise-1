9fe0aae47aa095e7045c9e3d9a6a14f5c719e41c
exercise <32698401+2017alan@users.noreply.github.com>
2018-01-13 13:57:13 +0800

    Update RL_QG_agent.py

--- 期末作业/RL_QG_agent.py
+++ 期末作业/RL_QG_agent.py
@@ -14,7 +14,7 @@ class RL_QG_agent:
         # 补全代码
 
 
-    def place(self,state,enables):
+    def place(self,state,enables,player):
         # 这个函数 主要用于测试， 返回的 action是 0-63 之间的一个数值，
         # action 表示的是 要下的位置。
         action = 123456789    # 删掉这句话，并填写相应代码
